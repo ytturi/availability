475b6b08c763752af70c07926fb23426732728db
Ytturi <ytturi@protonmail.com>
2019-11-15 14:08:17 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='python_pkg_template',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "ConfigParser",
+    ]
+    entry_points='''
+        [console_scripts]
+        do_smth=python_pkg_template.cli:do_smth
+    ''',
+)
